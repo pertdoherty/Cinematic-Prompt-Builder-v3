752b67a3ca5c323caceff629109fbd619fa1575b
pertdoherty <pert.cobain@gmail.com>
2026-02-26 15:15:17 +0800

    Fix blank page by implementing HashRouter and ensuring relative asset paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
